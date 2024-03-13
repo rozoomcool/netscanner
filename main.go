1ba297ef74015fb040d50658ce14f57f32a02c5b
rozoomcool <rozoom.um@gmail.com>
2024-03-13 06:23:20 +0300

    .

--- main.go
+++ main.go
@@ -9,47 +9,60 @@ import (
 )
 
 func main() {
-	// hosts := make(chan string)
-	ports := make(chan int)
-	defer close(ports)
-	// defer close(hosts)
+	hosts := make(chan string)
+	defer close(hosts)
 
-	// getEnabledHosts(hosts)
+	getEnabledHosts(hosts)
 
-	// for host := range hosts {
-	checkAddressPorts("localhost", ports)
-	// }
+	for host := range hosts {
+		if host == "" {
+			fmt.Println(host)
+			continue
+		} else {
+
+			ports := make(chan int)
+
+			func() {
+				defer close(ports)
+				checkAddressPorts(host, ports)
+			}()
+
+			fmt.Printf("HOST:: %v\n", host)
+			for i := range ports {
+				if i != 0 {
+					fmt.Printf("\tport %d is open\n", i)
+				}
+			}
+		}
+	}
 
 	// for host := range hosts {
 	// 	fmt.Printf("HOST::%v\n", host)
-	for i := range ports {
-		if i != 0 {
-			fmt.Printf("\tport %d is open\n", i)
-		}
-	}
 
 }
 
 func getEnabledHosts(hosts chan string) {
-	baseIP := getPrefId()
+	baseIP := getPrefIP()
 
-	go func() {
-		for i := 1; i <= 255; i++ {
+	for i := 1; i <= 255; i++ {
+		go func(i int) {
 			cmd := exec.Command("ping", "-c", "1", "-W", "1", fmt.Sprintf("%v:%d", baseIP, i))
-			if error := cmd.Run(); error != nil {
-				continue
+			if err := cmd.Run(); err != nil {
+				hosts <- ""
+				return
 			}
 
-			if error := cmd.Wait(); error != nil {
-				continue
+			if err := cmd.Wait(); err != nil {
+				hosts <- ""
+				return
 			}
 
 			hosts <- fmt.Sprintf("%v:%d", baseIP, i)
-		}
-	}()
+		}(i)
+	}
 }
 
-func getPrefId() string {
+func getPrefIP() string {
 	addr, err := GetOutboundIP()
 	if err != nil {
 		fmt.Println(err)
@@ -62,28 +75,25 @@ func getPrefId() string {
 	return baseIP
 }
 
-func checkAddressPorts(port string, result chan int) {
-	go func() {
-		for i := 1; i <= 40000; i++ {
-			conn, err := net.DialTimeout("tcp", fmt.Sprintf("%v:%d", port, i), time.Second*3)
+func checkAddressPorts(host string, result chan int) {
+	for i := 1; i <= 65536; i++ {
+		go func(i int) {
+			conn, err := net.DialTimeout("tcp", fmt.Sprintf("%v:%d", host, i), time.Second*3)
 			if err != nil {
-				result <- 0
 			} else {
 				conn.LocalAddr()
 				result <- i
 			}
-		}
-	}()
+		}(i)
+	}
 }
 
 func GetOutboundIP() (net.IP, error) {
-	for i := 1; i < 100; i++ {
-		conn, err := net.Dial("udp", "8.8.8.8:1")
-		if err == nil {
-			localAddr := conn.LocalAddr().(*net.UDPAddr)
-			return localAddr.IP, nil
-		}
-		defer conn.Close()
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err == nil {
+		localAddr := conn.LocalAddr().(*net.UDPAddr)
+		return localAddr.IP, nil
 	}
+	defer conn.Close()
 	return nil, fmt.Errorf("failed to get current ip address")
 }
